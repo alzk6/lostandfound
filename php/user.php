76253211e93af5808bdec157ed1b935810fb0352
alzk6 <alzk6@mst.edu>
2016-12-03 20:03:42 -0600

    I seriously have so many issues with php...
    these buttons should run the functions when clicked

--- php/user.php
+++ php/user.php
@@ -1,5 +1,14 @@
 <!DOCTYPE html>
 <html>
+
+ <form action="functs.php">
+    <input type="text" name="txt" />
+    <input type="submit" name="insert" value="Add Item" onclick="addEntry()" />
+    <input type="submit" name="select" value="Delete" onclick="deleteEntry" />
+    <input type="submit" name="select" value="Modify" onclick="editEntry" />
+  </form>
+
+
 <?php
 //this file will be the user web interface
 ?>
